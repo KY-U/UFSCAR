29e0af7ea246b2df3ebdda4f4c1fd562c562394e
KY-U <arcocaio10@hotmail.com>
2022-06-28 22:13:24 -0300

    Processos corrigidos, threads ainda restam dúvidas

--- 3_semestre/sistemas_operacionais/conta_processos.c
+++ 3_semestre/sistemas_operacionais/conta_processos.c
@@ -10,8 +10,10 @@
 #include <errno.h>
 #include <pthread.h>
 #include <time.h>
+#include <sys/wait.h>
+#include <syscall.h>
 
-//tarefa q nao faz nada pra servir de parametro pra create_thread()
+//tarefa que nao faz nada pra servir de parametro pra create_thread()
 void * tarefa(void *p){
     sleep(10);
     pthread_exit(0);
@@ -23,29 +25,35 @@ int main(int argc, char **argv){
     double tempo_proc, tempo_thread;
     int quant = 0, erro = 0, quant_proc;
     pid_t resultado;
-
     ini = clock();
     while(!erro){
         //processo nao foi criado
         if((resultado = fork()) == -1){
             printf("Criacao falhou no processo %d: %s\n", quant, strerror(errno));
             erro = 1;
-            exit(0);
+            break;
         }
         //proceso foi criado
-        else{
+        //processo pai
+        if(resultado != 0){
             quant++;
-            if(quant == 1000) fim = clock();
-            printf("Criado processo %d de pid %d\n", quant, getpid());
-            //interrompendo o loop no processo dos filhos
-            if(resultado == 0){ 
-                //sleep(10);    //da ruim
-                break;
-            }
+            printf("Criado processo %d de pid %d\n", quant, resultado);
+        }
+        //processo filho
+        else{
+            //interrompendo o loop no processo dos filhos 
+            sleep(10);    
+            exit(0);
         }
     }
+    fim = clock();
     quant_proc = quant;
     tempo_proc = ((double) (fim - ini) / CLOCKS_PER_SEC);
+
+    //esperando todos os processos encerrarem
+     for(int i = 0; i < quant; i++){
+        wait(NULL);
+    }
 
     //contando as threads
     quant = erro = 0;
@@ -61,16 +69,16 @@ int main(int argc, char **argv){
         //thread foi criada
         else{
             quant++;
-            if(quant == 1000) fim = clock();
-            printf("Criado thread %d PID %d\n", quant, getpid());
-        }
+            if(quant == quant_proc) fim = clock();
+            printf("Criado thread %d PID %ld\n", quant, pthread_self());       //Tentei printar tanto o LWP ID syscall(SYS_gettid)
+        }                                                                      //quanto o POSIX ID pthread_self() ambos mostram o mesmo ID toda vez
     }
     tempo_thread = ((double) (fim - ini) / CLOCKS_PER_SEC);
 
     //mostrando os resultados finais
     printf("Foi criado um total de %d processos\n", quant_proc);
     printf("Foi criado um total de %d threads\n", quant);
-    printf("Os primeiros 1000 processos e threads, demoraram, respectivamente:\n%f e %f segundos\n", tempo_proc, tempo_thread);
+    printf("Os primeiros %d processos e threads, demoraram, respectivamente:\n%f e %f segundos\n",quant_proc, tempo_proc, tempo_thread);
 
     return 0;
 }
