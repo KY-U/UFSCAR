2b58ad182bf6d9383c938e4005dd788ac164b472
KY-U <uscaio@estudante.ufscar.br>
2023-11-08 16:52:18 -0300

    exemplo da prof em json e formatação

--- 6_semestre/banco_de_dados_para_ciencia_de_dados/distintos modelos para prontuário eletrônico/exemplo.js
+++ 6_semestre/banco_de_dados_para_ciencia_de_dados/distintos modelos para prontuário eletrônico/exemplo.js
@@ -1,18 +1,31 @@
 db.pessoa.insert({
     cpf: '12345678911',
-    nome:'Joao Dois',
-    nroufscar:'1234445678',
+    nome: 'Joao Dois',
+    nroufscar: '1234445678',
     consultas: [
-        {data:'27-08-2019',
-        medico:'Mani Moura',
-        sintomas:['dor cabeca', 'coriza', 'tosse'],
-        diagnostico:['gripe', 'enxaqueca'],
-        exames:[ { nomeexame: 'Raio X da Face', data:'1-1-2020'}, {nomeexame:'tipagem sanguinea', laboratorio:'ufscarlab'}],
-        medicamentos:[{nome:'dipirona', dosagem: '3xdia'}, {nome: 'benegripe', dosagem: '1xsem'}]},
-
-        {data:'12-07-2019',
-        medico:'Joao Saude',
-        sintomas:['dor peito', 'suor', 'tosse'],
-        diagnostico:['pneumonia', 'h1n1'],
-        medicamentos:[{nome: 'dipirona', dosagem: '3xdia'}, {nome: 'benegripe',
-        dosagem: '1xsem'}]}]});
+        {   
+            data: '27-08-2019',
+            medico: 'Mani Moura',
+            sintomas: ['dor cabeca', 'coriza', 'tosse'],
+            diagnostico: ['gripe', 'enxaqueca'],
+            exames:[ 
+                { nomeexame: 'Raio X da Face', data: '1-1-2020'}, 
+                { nomeexame: 'tipagem sanguinea', laboratorio: 'ufscarlab'}
+            ],
+            medicamentos:[
+                { nome: 'dipirona', dosagem: '3xdia'}, 
+                { nome: 'benegripe', dosagem: '1xsem'}
+            ]
+        },
+        {   
+            data: '12-07-2019',
+            medico: 'Joao Saude',
+            sintomas: ['dor peito', 'suor', 'tosse'],
+            diagnostico: ['pneumonia', 'h1n1'],
+            medicamentos: [
+                { nome: 'dipirona', dosagem: '3xdia'}, 
+                { nome: 'benegripe',dosagem: '1xsem'}
+            ]
+        }
+    ]
+});
