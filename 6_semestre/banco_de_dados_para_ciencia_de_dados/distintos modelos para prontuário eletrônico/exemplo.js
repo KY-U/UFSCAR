d52122a3158857b9cdb9710462de91a70850e80c
KY-U <uscaio@estudante.ufscar.br>
2023-11-08 16:42:07 -0300

    exemplo da professora em js

---
+++ 6_semestre/banco_de_dados_para_ciencia_de_dados/distintos modelos para prontuário eletrônico/exemplo.js
@@ -0,0 +1,18 @@
+db.pessoa.insert({
+    cpf: '12345678911',
+    nome:'Joao Dois',
+    nroufscar:'1234445678',
+    consultas: [
+        {data:'27-08-2019',
+        medico:'Mani Moura',
+        sintomas:['dor cabeca', 'coriza', 'tosse'],
+        diagnostico:['gripe', 'enxaqueca'],
+        exames:[ { nomeexame: 'Raio X da Face', data:'1-1-2020'}, {nomeexame:'tipagem sanguinea', laboratorio:'ufscarlab'}],
+        medicamentos:[{nome:'dipirona', dosagem: '3xdia'}, {nome: 'benegripe', dosagem: '1xsem'}]},
+
+        {data:'12-07-2019',
+        medico:'Joao Saude',
+        sintomas:['dor peito', 'suor', 'tosse'],
+        diagnostico:['pneumonia', 'h1n1'],
+        medicamentos:[{nome: 'dipirona', dosagem: '3xdia'}, {nome: 'benegripe',
+        dosagem: '1xsem'}]}]});
